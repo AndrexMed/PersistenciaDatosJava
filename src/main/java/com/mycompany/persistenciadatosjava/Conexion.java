5a6a5eff27846d0966d6b41085e505fbb95c9267
AndrexMed <giovanniandrex@gmail.com>
2023-01-08 22:07:43 -0500

    Conexion de Mysql con Java

---
+++ src/main/java/com/mycompany/persistenciadatosjava/Conexion.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.persistenciadatosjava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author giova
+ */
+public class Conexion {
+    
+    public Connection get_connection(){
+        Connection conection = null;
+        try{
+            
+            conection= DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes.app", "root","");
+            if (conection != null) {
+                System.out.println("Conexion exitosa");
+            }
+            }catch(SQLException e){
+            System.out.println(e);
+        }
+        return conection;
+    }
+}
